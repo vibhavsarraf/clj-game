a00923a45940689e11120befe53f2de8a77fc827
vibhavsarraf <vidgaf@gmail.com>
2019-10-31 00:24:21 +0530

    updated for 2 players

--- src/cljs_game/core.cljs
+++ src/cljs_game/core.cljs
@@ -17,25 +17,32 @@
 
 (def pi (. js/Math -PI))
 
-(def ball1 {:id 1 :pos [20 20] :vel [2 2] :radius 15 :mass (* 15 15)})
+(def ball1 {:id 1 :pos [20 20] :vel [2 2] :radius 15 :mass (* 15 15) :player 1 :color "#FF0000"})
 
-(def ball2 {:id 2 :pos [90 20] :vel [0 0] :radius 15 :mass (* 15 15)})
+(def ball2 {:id 2 :pos [90 20] :vel [0 0] :radius 15 :mass (* 15 15) :player 1 :color "#FF0000"})
 
-(def ball3 {:id 3 :pos [50 90] :vel [0 0] :radius 15 :mass (* 15 15)})
+(def ball3 {:id 3 :pos [50 200] :vel [0 0] :radius 15 :mass (* 15 15) :player 1 :color "#FF0000"})
 ;
-;(def ball4 {:id 4 :pos [90 20] :vel [1 1] :radius 15 :mass (* 15 15)})
+(def ball4 {:id 4 :pos [100 90] :vel [1 1] :radius 15 :mass (* 15 15) :player 2 :color "#00FF00"})
 
-(def starting-state {:balls [ball1 ball2 ball3]
-                     :stable? false})
+(def ball5 {:id 5 :pos [120 20] :vel [1 1] :radius 15 :mass (* 15 15) :player 2 :color "#00FF00"})
+
+(def ball6 {:id 6 :pos [200 20] :vel [1 1] :radius 15 :mass (* 15 15) :player 2 :color "#00FF00"})
+
+(def special-ball {:id 7 :pos [200 200] :vel [1 1] :radius 10 :mass (* 10 10) :player 0 :special? 2 :color "#0000FF"})
+
+(def starting-state {:balls [ball1 ball2 ball3 ball4 ball5 ball6 special-ball]
+                     :stable? false
+                     :player-turn 1})
 
 (defonce world-state (atom starting-state))
 
-(defn draw-ball [ctx {:keys [pos radius]}]
+(defn draw-ball [ctx {:keys [pos radius color]}]
   ;(js/console.log "draw-ball called")
   (let [[posx posy] pos]
     (.beginPath ctx)
     (.arc ctx posx posy radius 0 (* 2 pi))
-    (set! (.. ctx -fillStyle) "#FF0000")
+    (set! (.. ctx -fillStyle) color)
     (.fill ctx)
     (.closePath ctx)))
 
@@ -153,7 +160,7 @@
       b1)))
 
 (defn new-vel [[vx vy] damp]
-  (let [f (fn [a] (if (> (abs a) 0.001) (* a damp) 0))]
+  (let [f (fn [a] (if (> (abs a) 0.01) (* a damp) 0))]
     [(f vx) (f vy)]))
 
 (defn update-ball-vecs [{:keys [vel] :as ball}]
@@ -190,8 +197,6 @@
           (assoc :balls updated-balls)
           (assoc :stable? (balls-stable? updated-balls))))))
 
-(reset! world-state starting-state)
-
 (def init-action {:pressed? false
                   :start-pos [0 0]
                   :end-pos [0 0]})
@@ -207,16 +212,30 @@
       (assoc ball :vel (scaler-mul-vector (sub-vector s e) 0.05))
       ball)))
 
+(defn get-ball-moved [balls action]
+  (let [total-balls (count balls)
+        s (:start-pos action)]
+    (loop [x 0]
+      ;(println x)
+      (if (< x total-balls)
+        (if (pos-in-ball? s (get balls x))
+          x
+          (recur (inc x)))
+        nil))))
+
+(defn change-player [cur-player]
+  (if (= 1 cur-player) 2 1))
+
 (defn apply-action-state [action state]
-  (js/console.log "applying action" (str action) (str state))
-  (let [balls (:balls state)
-        upd-balls (loop [updated-balls '() rest-balls balls]
-                    (if (first rest-balls)
-                      (recur
-                        (conj updated-balls (apply-action-ball action (first rest-balls)))
-                        (rest rest-balls))
-                      updated-balls))]
-    (assoc state :balls (vec upd-balls))))
+  (let [{balls :balls player-turn :player-turn} state
+        ind (get-ball-moved balls action)
+        ball-moved (get balls ind)]
+    (if (and (not= ind nil) (= player-turn (:player ball-moved)))
+      (-> state
+          (assoc-in [:balls ind] (apply-action-ball action ball-moved))
+          (assoc :stable? false)
+          (update :player-turn change-player))
+      state)))
 
 (set! (.. js/document -body -onmousedown) (fn [e]
                                  (let [mouse-pos [(.. e -x) (.. e -y)]]
@@ -228,8 +247,12 @@
                                             (let [mouse-pos [(.. e -x) (.. e -y)]]
                                               (js/console.log e)
                                               (swap! action assoc :end-pos mouse-pos)
-                                              (swap! world-state assoc :stable? false)
                                               (swap! world-state #(apply-action-state @action %))))))
 
-(js/setInterval (fn []
-                  (reset! world-state (update-state @world-state))) 15)
+(defonce screen-loaded? (atom false))
+
+(when (not @screen-loaded?)
+  (reset! screen-loaded? true)
+  (reset! world-state starting-state)
+  (js/setInterval (fn []
+                    (reset! world-state (update-state @world-state))) 15))
